3f973eab2f0f20c261a930b06b8476323df612c8
Abdul Rahman <abdulnsamba@gmail.com>
2024-02-08 13:42:58 +0300

    added url links

--- models/routes.py
+++ models/routes.py
@@ -36,9 +36,9 @@ quotes = [
 def home():
     return render_template("home.html", quotes=quotes)
 
-@app.route("/about")
-def about():
-    return render_template("about.html", title="About")
+# @app.route("/about")
+# def about():
+#     return render_template("about.html", title="About")
 
 @app.route("/register", methods=('GET', 'POST'))
 def register():
