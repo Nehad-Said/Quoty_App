f98ef3afa34ebd2e023e5a2306af53a4bc2bb789
Abdul Rahman <abdulnsamba@gmail.com>
2024-02-27 12:47:29 +0300

    added styles

--- models/error.py
+++ models/error.py
@@ -2,12 +2,14 @@ from flask import render_template
 from models import app, db
 
 
+# Handle 404 not found error with an html page.
 @app.errorhandler(404)
 def not_found(error):
-	return render_template('not_found.html'), 404
+	return render_template('not_found.html', error=error), 404
 
-
+# Handle 500 not found error with an html page.
 @app.errorhandler(500)
 def not_found(error):
-	db.session.rollback()
-	return render_template('server_error.html'), 500
+	if db.session.is_active:
+		db.session.rollback()
+	return render_template('server_error.html', error=error), 500
--- models/utils.py
+++ models/utils.py
@@ -1,25 +1,32 @@
 import requests
+from flask import abort
 from datetime import datetime
 import pytz
 from models.db_models import Quote
 
 
+# Get quotes from the databases.
 def get_quotes(page, order_by='created_on', descending=True):
-    if descending:
-        q = Quote.query.order_by(getattr(Quote, order_by).desc()).paginate(page=page, per_page=7)
-    else:
-        q = Quote.query.order_by(getattr(Quote, order_by)).paginate(page=page, per_page=7)
-    return q
+    try:
+        if descending:
+            q = Quote.query.order_by(getattr(Quote, order_by).desc()).paginate(page=page, per_page=5)
+        else:
+            q = Quote.query.order_by(getattr(Quote, order_by)).paginate(page=page, per_page=5)
+        return q
+    except Exception as e:
+        abort(500, description="Database couldn't be reached.")
 
-
+# Query the database for random quotes from an external API
 def getRandQuote():
-	r = requests.get('https://api.quotable.io/quotes/random/?limit=5')
-	if r:
-		return r.json()
-	else:
-		return {"content": "None", "author":"None"}
+    try:
+        r = requests.get('https://api.quotable.io/quotes/random/?limit=4')
+        r.raise_for_status()
+        return r.json()
+    except requests.exceptions.RequestException as e:
+        abort(400, description="Couldn't reach external server, try again later.!")
       
 
+# Format the date to get elapsed time
 def format_post_date(post_date, timezone='UTC'):
     current_datetime_utc = datetime.utcnow().replace(tzinfo=pytz.utc)
     post_date_utc = post_date.replace(tzinfo=pytz.timezone(timezone))
@@ -31,13 +38,15 @@ def format_post_date(post_date, timezone='UTC'):
         seconds = time_difference.seconds
 
         if seconds < 60:
-            return f"{seconds} seconds ago"
+            return f"{seconds}sec ago"
         elif seconds < 3600:
             minutes = seconds // 60
-            return f"{minutes} minutes ago"
+            return f"{minutes}min ago"
         else:
             hours = seconds // 3600
-            return f"{hours} hours ago"
+            if hours == 1:
+                return f"{hours}hr ago"
+            return f"{hours}hrs ago"
     else:
         # If the post is from a different day, display day and month
         return post_date.strftime("%b %d")
